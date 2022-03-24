0f9c9ad8385a6d27ddf8a4b0b05cdbdf8bce67f7
신병철 <mousecjf@gmail.com>
2022-03-24 10:54:46 +0900

    Add files via upload

--- Hash/전화번호 목록/PhoneBook.java
+++ Hash/전화번호 목록/PhoneBook.java
@@ -12,6 +12,9 @@ public class PhoneBook{
         for(int i = 0; i < phone_book.length; i++) {
             for(int j = 0; j < phone_book[i].length(); j++) {
                 if(map.containsKey(phone_book[i].substring(0,j))) {
+					// containsKey 는 Map 에 키 값이 있는지 확인하는 메서드이다. 
+					// 인자로 보낸 키 값이 있으면 true 를 반환하고, 없으면 false 를 반환한다.
+					// cf) value 값을 확인하고 싶은 경우에는 containsValue 메서드를 사용한다.
                     answer = false;
                     return answer;
                 }
