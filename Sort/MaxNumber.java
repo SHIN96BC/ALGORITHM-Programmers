2c9ba60bfc2ea0ba671cab4a5fb1fc0ed7bbecab
신병철 <mousecjf@gmail.com>
2022-05-23 21:24:26 +0900

    Update MaxNumber.java

--- Sort/MaxNumber.java
+++ Sort/MaxNumber.java
@@ -5,6 +5,10 @@ class Solution {
     public String solution(int[] numbers) {
         String answer = "";
         
+        // for 문이 3개나 있다보니 시간초과가 발생한다. int배열을 그대로 2중포문으로 돌리고, 
+        // findMaxNum 메서드에서 String 으로 변환해서 해보자!
+        
+        
         // PriorityQueue<String> priQ = new PriorityQueue<>(Collections.reverseOrder());
         String[] numberStrArr = new String[numbers.length];
         for(int i=0; i<numbers.length; i++) {
@@ -41,12 +45,34 @@ class Solution {
         }
         
         if(xNum == yNum) {
-            // 길이가 더 긴 쪽이랑 loop 가 같아지면 어떻게 하나 똑같다는 의미이므로 false를 반환한다.
-            if(x.length() >= y.length()) {
-                if(x.length() == loop) return false;
-            }else if(x.length() < y.length()) {
-                if(y.length() == loop) return false;
+            // x 가 마지막 숫자이면 y의 다음 숫자가 있는지 확인하고 그 숫자와 비교해서 y가 더 크면 true 를 반환한다. 
+            if(x.length() == loop+1) {
+                if(y.length() > loop+1) {
+                    yNum = Integer.parseInt(y.substring(loop+1, loop+2));
+                    if(xNum < yNum) {
+                        return true;
+                    }
+                    return false;
+                }else {
+                    return false;
+                }
+            }else if(y.length() == loop+1) {
+                // y 가 마지막 숫자이면 x의 다음 숫자가 있는지 확인하고 그 숫자와 비교해서 y가 더 크면 true 를 반환한다. 
+                if(x.length() > loop+1) {
+                    xNum = Integer.parseInt(x.substring(loop+1, loop+2));
+                    if(xNum < yNum) {
+                        return true;
+                    }
+                    return false;
+                }else {
+                    return false;
+                }
             }
+
+            // 해결: if문을 이용해서 문자열의 길이가 loop + 1 과 같으면 문자열 길이가
+            // 초과 되므로 더 이상 재귀하지않고 return 하게 한다.(조건은 x가 loop + 1을 
+
+            // 초과 했을 때, y가 loop + 1 을 초과 했을 때, 둘다 아닐 때)
             return findMaxNum(x,y,loop+1);
         }else if(xNum < yNum) {
             return true;
@@ -55,4 +81,4 @@ class Solution {
         }
     }
 }
-// 런타임 에러가 발생하고 있는데 아마도 저의 생각은 loop+1 때문에 길이가 초과된게 아닌가 싶습니다.길이에따라 제한이 필요할 것 같습니다.
+// 런타임 에러가 발생하고 있는데 아마도 저의 생각은 loop+1 때문에 길이가 초과된게 아닌가 싶습니다.길이에따라 제한이 필요할 것 같다
