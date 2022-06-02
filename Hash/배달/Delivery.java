9d9518171a36942ad8c95ee50714ac75736a74cd
신병철 <mousecjf@gmail.com>
2022-06-03 08:30:21 +0900

    Update Delivery.java

--- Hash/배달/Delivery.java
+++ Hash/배달/Delivery.java
@@ -1,4 +1,4 @@
-class Solution {
+class Delivery {
     public int solution(int N, int[][] road, int K) {
         // N == 마을의 개수, 
         // read == [마을번호1, 마을번호2, 두마을을 연결하는 도로를 지나는데 걸리는 시간], 
@@ -11,6 +11,12 @@ class Solution {
         
         // Map 을 사용할지 배열을 그대로 가지고 풀지 생각중이다.
         int answer = 0;
+        
+        int[] intArr = new int[N];
+        
+        for(int i = 0; i < road.length; i++) {
+            
+        }
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("Hello Java");
