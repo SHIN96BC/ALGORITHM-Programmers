ff25c6510b569c0d4fb44bbc2ec3f7dd8d450728
신병철 <mousecjf@gmail.com>
2022-06-02 21:09:02 +0900

    Update SumDigit.java

--- Practice/자릿수 더하기/SumDigit.java
+++ Practice/자릿수 더하기/SumDigit.java
@@ -7,6 +7,8 @@ public class SumDigit {
         String nStr = Integer.toString(n);
         
         for(int i = 0; i < nStr.length(); i++) {
+            // charAt 메서드를 사용해서 문자열을 하나씩 잘라냅니다.
+            // Character 의 getNumericValue 메서드를 사용해서 char 타입을 int 타입으로 바꿔줍니다.
             answer += Character.getNumericValue(nStr.charAt(i));
         }
 
