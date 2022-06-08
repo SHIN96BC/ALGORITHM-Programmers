6cd6ce7fbde5561e43359076906cfce74bfb7001
ByeongCheol Shin <mousecjf@gmail.com>
2022-06-08 13:06:27 +0900

    Create ReportResult.java

---
+++ Hash/신고 결과 받기/ReportResult.java
@@ -0,0 +1,12 @@
+class ReportResult {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        
+        // 유저들이 몇번 신고를 당했는지 계산합니다.(한사람이 여러번 신고했을 때는 1회로 처리합니다.)
+        
+        
+        // 처리결과를 몇 회 받는지 아이디 별로 체크해서 Map 에 저장합니다.
+        
+        return answer;
+    }
+}
