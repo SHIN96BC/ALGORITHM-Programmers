27282c8f677a3f7b2c876cd6db68824ab7fedbb1
ByeongCheol Shin <mousecjf@gmail.com>
2022-06-24 15:44:43 +0900

    Update MaxNumber.java

--- Sort/가장 큰 수/MaxNumber.java
+++ Sort/가장 큰 수/MaxNumber.java
@@ -1,5 +1,22 @@
 class MaxNumber {
-    public String solution(int[] numbers) {
+     public String solution(int[] numbers) {
+        String answer = "";
+        PriorityQueue<String> priQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 우선순위 큐를 이용해서 String 형태로 우선순위를 역순으로 정렬합니다.
+        for(int i=0; i<numbers.length; i++) {
+           priQ.add(Integer.toString(numbers[i]));
+        }
         
+        // 배열이 모두 0 일때는 0을 하나만 반환합니다.
+        if(priQ.peek().equals("0")) return "0";
+        
+        // 출력용
+        while(priQ.size() > 0) {
+            String str = priQ.poll();
+            answer += str;
+            System.out.println(str);
+        }
+        return answer;
     }
 }
