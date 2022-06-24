1ec218e2fc25184c0e4bc6b94b4a68de026e52c2
ByeongCheol Shin <mousecjf@gmail.com>
2022-06-24 17:32:56 +0900

    60점 시간초과 없음

--- Sort/가장 큰 수/MaxNumber.java
+++ Sort/가장 큰 수/MaxNumber.java
@@ -1,5 +1,9 @@
 class MaxNumber {
-     public String solution(int[] numbers) {
+    public static final int FIRST_NUMBER_MAX = -1;
+    public static final int LAST_NUMBER_MAX = 1;
+    public static final int EXCEPTION = 0;
+    
+    public String solution(int[] numbers) {
         String answer = "";
         PriorityQueue<String> priQ = new PriorityQueue<>(Collections.reverseOrder());
 
@@ -11,12 +15,47 @@ class MaxNumber {
         // 배열이 모두 0 일때는 0을 하나만 반환합니다.
         if(priQ.peek().equals("0")) return "0";
         
-        // 출력용
+        // 큐에서 하나씩 꺼내서 checkMaxNumberStr 메서드의 결과에 따라 다르게 처리해줍니다.
+        String lastNumber = null;
         while(priQ.size() > 0) {
-            String str = priQ.poll();
-            answer += str;
-            System.out.println(str);
+            String firstNumber = priQ.poll();
+            
+            // tempStr 이 null 일때 poll 을 한번 합니다.
+            if(lastNumber == null) lastNumber = priQ.poll();
+            
+            int result = checkMaxNumberStr(firstNumber, lastNumber);
+            
+            switch(result) {
+                case FIRST_NUMBER_MAX:
+                    answer += firstNumber;
+                    break;
+                case LAST_NUMBER_MAX:
+                    answer += lastNumber;
+                    lastNumber = firstNumber;
+                    break;
+                default:
+            }
         }
+        answer += lastNumber;
         return answer;
+    }
+    
+    // 두 문자열을 서로 앞뒤로 붙여서 비교했을 때 어느쪽이 더 큰지 확인하는 메서드 입니다.
+    private int checkMaxNumberStr(String str1, String str2) {
+        if(str1 == null && str2 == null) {
+            return EXCEPTION;
+        }else if(str1 == null) {
+            return LAST_NUMBER_MAX;
+        }else if(str2 == null) {
+            return FIRST_NUMBER_MAX;
+        }
+        String tempStr = str1 + str2;
+        // String 클래스의 compareTo()  메서드를 사용해서 사전순으로 비교합니다.
+        // 파라미터의 스트링값이 더 크면 음수, 같으면 0, 작으면 양수
+        if(tempStr.compareTo((str2+str1)) > 0) {
+            return FIRST_NUMBER_MAX;
+        }else {
+            return LAST_NUMBER_MAX;
+        }
     }
 }
