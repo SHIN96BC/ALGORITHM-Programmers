4e889c6d349e073ede40940441361534bdf6c72f
ByeongCheol Shin <mousecjf@gmail.com>
2022-06-30 09:08:28 +0900

    성공

--- SQL/Oracle/Join/있었는데요 없었습니다.sql
+++ SQL/Oracle/Join/있었는데요 없었습니다.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
+-- 이너 조인을 사용해서 ins 테이블의 datetime 보다 outs 테이블의 datetime 이 더 작은 데이터들을 조회합니다.
 SELECT ain.animal_id, ain.name 
 FROM animal_ins ain, animal_outs aout
 WHERE ain.datetime > aout.datetime 
