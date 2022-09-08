5fa25a8d62ce40d2a23d63a00f43a5d6f4bb992c
ByeongCheol Shin <mousecjf@gmail.com>
2022-09-08 20:18:31 +0900

    성공

---
+++ Practice/서울에서 김서방 찾기/FindByKim.java
@@ -0,0 +1,13 @@
+class FindByKim {
+    public String solution(String[] seoul) {
+        String answer = "";
+        
+        for(int i = 0; i < seoul.length; i++) {
+            if("Kim".equals(seoul[i])) {
+                answer = "김서방은 " + i + "에 있다";
+            }
+        }
+        
+        return answer;
+    }
+}
