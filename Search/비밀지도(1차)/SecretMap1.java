c588f86f08c70be5bd998f2c15d9b0ccc500d45f
ByeongCheol Shin <mousecjf@gmail.com>
2022-12-16 15:40:10 +0900

    실패 75점

--- Search/비밀지도(1차)/SecretMap1.java
+++ Search/비밀지도(1차)/SecretMap1.java
@@ -1,6 +1,28 @@
 class SecretMap1 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-        return answer;
+        String[] answer = new String[n];
+        
+// #방법1 실패 75점 2번 6번 런타임 에러 발생
+//         String result = "";
+//         for (int i = 0; i < n; i++) {
+//             String arr1Binary = String.format("%0" + n + "d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
+//             String arr2Binary = String.format("%0" + n + "d", Integer.parseInt(Integer.toBinaryString(arr2[i])));
+            
+//             for (int j = 0; j < n; j++) {
+//                 if (arr1Binary.charAt(j) == 48 && arr2Binary.charAt(j) == 48) {
+//                     result += " ";
+//                 } else {
+//                     result += "#";
+//                 }
+//             }
+            
+//             answer[i] = result;
+//             result = "";
+//         }
+        
+//         return answer;
+        
+// #방법2 
+        
     }
 }
