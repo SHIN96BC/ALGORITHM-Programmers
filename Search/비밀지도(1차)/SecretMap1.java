7ba78e09ab3d969d445710c11634ddd45409972a
ByeongCheol Shin <mousecjf@gmail.com>
2022-12-16 16:02:04 +0900

    성공

--- Search/비밀지도(1차)/SecretMap1.java
+++ Search/비밀지도(1차)/SecretMap1.java
@@ -1,8 +1,9 @@
 class SecretMap1 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        
+
 // #방법1 실패 75점 테스트 2번 6번 런타임 에러 발생
+//         String[] answer = new String[n];
+        
 //         String result = "";
 //         for (int i = 0; i < n; i++) {
 //             String arr1Binary = String.format("%0" + n + "d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
@@ -22,7 +23,26 @@ class SecretMap1 {
         
 //         return answer;
         
-// #방법2 
+// #방법2 성공 100점 런타임 에러 원인은 int의 범위초과 문제였습니다. Long으로 바꾸어서 해결했습니다.
+        String[] answer = new String[n];
         
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            String arr1Binary = String.format("%0" + n + "d", Long.parseLong(Integer.toBinaryString(arr1[i])));
+            String arr2Binary = String.format("%0" + n + "d", Long.parseLong(Integer.toBinaryString(arr2[i])));
+            
+            for (int j = 0; j < n; j++) {
+                if (arr1Binary.charAt(j) == 48 && arr2Binary.charAt(j) == 48) {
+                    result += " ";
+                } else {
+                    result += "#";
+                }
+            }
+            
+            answer[i] = result;
+            result = "";
+        }
+        
+        return answer;
     }
 }
