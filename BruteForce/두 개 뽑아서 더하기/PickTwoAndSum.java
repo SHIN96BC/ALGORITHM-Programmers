be1b664a360a51e3c2b8c894cd688044f4548a06
ByeongCheol Shin <mousecjf@gmail.com>
2023-01-28 18:59:59 +0900

    성공

--- BruteForce/두 개 뽑아서 더하기/PickTwoAndSum.java
+++ BruteForce/두 개 뽑아서 더하기/PickTwoAndSum.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 class PickTwoAndSum {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
+        List<Integer> sumList = new ArrayList<>();
         
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j <numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+                // 중복 체크
+                if (sumList.indexOf(sum) < 0) {
+                    sumList.add(sum);
+                }
+            }
+        }
         
+        int[] answer = new int[sumList.size()];
+        for (int i = 0; i < sumList.size(); i++) {
+            answer[i] = sumList.get(i);
+        }
+        
+        // 오름차순 정렬
+        Arrays.sort(answer);
         
         return answer;
     }
