792509e35c277af74b553f7a96c431e25ce1b3e9
ByeongCheol Shin <mousecjf@gmail.com>
2023-01-29 20:21:28 +0900

    성공

--- Sorting/3진법 뒤집기/TernaryBase3Reverse.java
+++ Sorting/3진법 뒤집기/TernaryBase3Reverse.java
@@ -2,7 +2,18 @@ class TernaryBase3Reverse {
     public int solution(int n) {
         int answer = 0;
         
+        // 3진법 변환
+        String ternaryN = Integer.toString(n, 3);
         
+        StringBuffer sb = new StringBuffer();
+        
+        // 앞뒤 반전
+        for (int i = 0; i < ternaryN.length(); i++) {
+            sb.insert(0, ternaryN.substring(i, i+1));
+        }
+        
+        // 10진법 변환
+        answer = Integer.parseInt(sb.toString(), 3);
         
         return answer;
     }
