3fe376e3816a9c1eee21fa0e079886d08cc762d8
ByeongCheol Shin <mousecjf@gmail.com>
2023-02-02 08:55:35 +0900

    성공

--- BruteForce/약수의 개수와 덧셈/DivisorPlusAndMinus.java
+++ BruteForce/약수의 개수와 덧셈/DivisorPlusAndMinus.java
@@ -1,8 +1,23 @@
-class Solution {
+class DivisorPlusAndMinus {
     public int solution(int left, int right) {
         int answer = 0;
         
-        
+        for (int i = left; i <= right; i++) {
+            int divisorCount = 0;
+            
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisorCount++;
+                }
+            }
+            
+            // 짝수일 때는 더하고, 홀수일 때는 뺀다.
+            if (divisorCount % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
         
         return answer;
     }
