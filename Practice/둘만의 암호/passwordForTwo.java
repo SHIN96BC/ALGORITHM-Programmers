bbd90bf8ce7d3820d15dda0be6bc420b373c49c7
ByeongCheol Shin <mousecjf@gmail.com>
2023-02-11 22:53:41 +0900

    실패 21.1점 원인 분석중

--- Practice/둘만의 암호/passwordForTwo.java
+++ Practice/둘만의 암호/passwordForTwo.java
@@ -1,9 +1,38 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-      
-      
-      
-        return answer;
+        StringBuffer answer = new StringBuffer();
+        
+        for (int i = 0; i < s.length(); i++) {
+            answer.append(getNextAlphabet(s.charAt(i), index, skip));
+        }
+        
+        return answer.toString();
+    }
+    
+    private char getNextAlphabet(char c, int moveIndex, String skip) {
+
+        int skipCount = 0;
+        for (int i = 1; i <= moveIndex; i++) {
+            char checkAlphabet = (char)(c + i);
+            String alphabet = Character.toString(checkOutOfRange(checkAlphabet));
+
+            if (skip.contains(alphabet)) {
+                skipCount++;
+            }
+        }
+
+        char movedAlphabet = (char)(c + moveIndex + skipCount);
+
+        return checkOutOfRange(movedAlphabet);
+    }
+    
+    private char checkOutOfRange(char alphabet) {
+         // 아스키코드 a ~ z == 97 ~ 122
+        
+        if (alphabet > 122) {
+            return (char)('a' + alphabet - 123);
+        } else {
+            return alphabet;
+        }
     }
 }
